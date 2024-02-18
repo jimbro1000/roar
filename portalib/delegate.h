966d4c840a4db656a56f9caa89e390c8d424e5cf
Ciaran Anscomb <xroar@6809.org.uk>
2024-02-18 21:27:19 +0000

    delegates: extend macros up to 6 arguments

--- portalib/delegate.h
+++ portalib/delegate.h
@@ -32,6 +32,8 @@
 #define DELEGATE_S2(T,T0,T1) struct { T (*func)(void *, T0, T1); void *sptr; }
 #define DELEGATE_S3(T,T0,T1,T2) struct { T (*func)(void *, T0, T1, T2); void *sptr; }
 #define DELEGATE_S4(T,T0,T1,T2,T3) struct { T (*func)(void *, T0, T1, T2, T3); void *sptr; }
+#define DELEGATE_S5(T,T0,T1,T2,T3,T4) struct { T (*func)(void *, T0, T1, T2, T3, T4); void *sptr; }
+#define DELEGATE_S6(T,T0,T1,T2,T3,T4,T5) struct { T (*func)(void *, T0, T1, T2, T3, T4, T5); void *sptr; }
 
 /* Type name for delegates. */
 
@@ -40,6 +42,8 @@
 #define DELEGATE_T2(N,N0,N1) delegate_##N##_##N0##_##N1
 #define DELEGATE_T3(N,N0,N1,N2) delegate_##N##_##N0##_##N1##_##N2
 #define DELEGATE_T4(N,N0,N1,N2,N3) delegate_##N##_##N0##_##N1##_##N2##_##N3
+#define DELEGATE_T5(N,N0,N1,N2,N3,N4) delegate_##N##_##N0##_##N1##_##N2##_##N3##_##N4
+#define DELEGATE_T6(N,N0,N1,N2,N3,N5) delegate_##N##_##N0##_##N1##_##N2##_##N3##_##N4##_##N5
 
 /* Define a set of delegate types. */
 
@@ -94,6 +98,8 @@ typedef DELEGATE_S3(float, uint32_t, int, float *) DELEGATE_T3(float, uint32, in
 #define DELEGATE_AS2(N,N0,N1,f,s) (DELEGATE_T2(N,N0,N1))DELEGATE_INIT(f,s)
 #define DELEGATE_AS3(N,N0,N1,N2,f,s) (DELEGATE_T3(N,N0,N1,N2))DELEGATE_INIT(f,s)
 #define DELEGATE_AS4(N,N0,N1,N2,N3,f,s) (DELEGATE_T4(N,N0,N1,N2,N3))DELEGATE_INIT(f,s)
+#define DELEGATE_AS5(N,N0,N1,N2,N3,N4,f,s) (DELEGATE_T5(N,N0,N1,N2,N3,N4))DELEGATE_INIT(f,s)
+#define DELEGATE_AS6(N,N0,N1,N2,N3,N4,N5,f,s) (DELEGATE_T6(N,N0,N1,N2,N3,N4,N5))DELEGATE_INIT(f,s)
 
 /* Delegate default function names. */
 
@@ -102,6 +108,8 @@ typedef DELEGATE_S3(float, uint32_t, int, float *) DELEGATE_T3(float, uint32, in
 #define DELEGATE_DEFAULT_F2(N,N0,N1) delegate_##N##_default_##N0##_##N1
 #define DELEGATE_DEFAULT_F3(N,N0,N1,N2) delegate_##N##_default_##N0##_##N1##_##N2
 #define DELEGATE_DEFAULT_F4(N,N0,N1,N2,N3) delegate_##N##_default_##N0##_##N1##_##N2##_##N3
+#define DELEGATE_DEFAULT_F5(N,N0,N1,N2,N3,N4) delegate_##N##_default_##N0##_##N1##_##N2##_##N3##_##N4
+#define DELEGATE_DEFAULT_F6(N,N0,N1,N2,N3,N4,N5) delegate_##N##_default_##N0##_##N1##_##N2##_##N3##_##N4##_##N5
 
 /* Default no-op functions for defined delegate types. */
 
@@ -116,6 +124,8 @@ typedef DELEGATE_S3(float, uint32_t, int, float *) DELEGATE_T3(float, uint32, in
 #define DELEGATE_DEF_FUNC2(T,N,T0,N0,T1,N1,R) T DELEGATE_DEFAULT_F2(N,N0,N1)(void *sptr, T0 v0, T1 v1) { (void)sptr; (void)v0; (void)v1; return R; }
 #define DELEGATE_DEF_FUNC3(T,N,T0,N0,T1,N1,T2,N2,R) T DELEGATE_DEFAULT_F3(N,N0,N1,N2)(void *sptr, T0 v0, T1 v1, T2 v2) { (void)sptr; (void)v0; (void)v1; (void)v2; return R; }
 #define DELEGATE_DEF_FUNC4(T,N,T0,N0,T1,N1,T2,N2,T3,N3,R) T DELEGATE_DEFAULT_F4(N,N0,N1,N2,N3)(void *sptr, T0 v0, T1 v1, T2 v2, T3 v3) { (void)sptr; (void)v0; (void)v1; (void)v2; (void)v3; return R; }
+#define DELEGATE_DEF_FUNC5(T,N,T0,N0,T1,N1,T2,N2,T3,N3,T4,N4,R) T DELEGATE_DEFAULT_F5(N,N0,N1,N2,N3,N4)(void *sptr, T0 v0, T1 v1, T2 v2, T3 v3, T4 v4) { (void)sptr; (void)v0; (void)v1; (void)v2; (void)v3; (void)v4; return R; }
+#define DELEGATE_DEF_FUNC6(T,N,T0,N0,T1,N1,T2,N2,T3,N3,T4,N4,T5,N5,R) T DELEGATE_DEFAULT_F6(N,N0,N1,N2,N3,N4,N5)(void *sptr, T0 v0, T1 v1, T2 v2, T3 v3, T4 v4, T5 v5) { (void)sptr; (void)v0; (void)v1; (void)v2; (void)v3; (void)v4; (void)v5; return R; }
 
 DELEGATE_DEF_PROTO0(void, void);
 DELEGATE_DEF_PROTO1(void, void, _Bool, bool);
@@ -162,6 +172,8 @@ DELEGATE_DEF_PROTO3(float, float, uint32_t, uint32, int, int, float *, floatp);
 #define DELEGATE_DEFAULT2(N,N0,N1) DELEGATE_AS2(N, N0, N1, DELEGATE_DEFAULT_F2(N, N0, N1), NULL)
 #define DELEGATE_DEFAULT3(N,N0,N1,N2) DELEGATE_AS3(N, N0, N1, N2, DELEGATE_DEFAULT_F3(N, N0, N1, N2), NULL)
 #define DELEGATE_DEFAULT4(N,N0,N1,N2,N3) DELEGATE_AS4(N, N0, N1, N2, N3, DELEGATE_DEFAULT_F4(N, N0, N1, N2, N3), NULL)
+#define DELEGATE_DEFAULT5(N,N0,N1,N2,N3,N4) DELEGATE_AS5(N, N0, N1, N2, N3, N4, DELEGATE_DEFAULT_F5(N, N0, N1, N2, N3, N4), NULL)
+#define DELEGATE_DEFAULT6(N,N0,N1,N2,N3,N4,N5) DELEGATE_AS6(N, N0, N1, N2, N3, N4, N5, DELEGATE_DEFAULT_F6(N, N0, N1, N2, N3, N4, N5), NULL)
 
 /* Calling interface. */
 
