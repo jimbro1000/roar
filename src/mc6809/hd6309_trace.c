d01523fe4fb35a85dc009e35e02170e5d658f02a
Ciaran Anscomb <xroar@6809.org.uk>
2024-06-09 10:50:41 +0100

    mc6809/hd6309: fix trace disassembly of ,PCR

--- src/mc6809/hd6309_trace.c
+++ src/mc6809/hd6309_trace.c
@@ -928,8 +928,8 @@ static char const * const idx_fmts[17] = {
 	"%s$%04x,%s%s",
 	"%sF,%s%s",
 	"%sD,%s%s",
-	"%s%s$%02x,PCR%s",
-	"%s$%04x,PCR%s",
+	"%s<$%04x,PCR%s",
+	"%s>$%04x,PCR%s",
 	"%sW,%s%s",
 	"%s$%04X%s",
 	"%s%d,%s%s",
@@ -1286,10 +1286,10 @@ void hd6309_trace_byte(struct hd6309_trace *tracer, uint8_t byte, uint16_t pc) {
 				snprintf(tmp_text, sizeof(tmp_text), idx_fmts[tracer->idx_mode], pre, tracer->value, tracer->idx_reg, post);
 				break;
 			case IDX_PCR8:
-				snprintf(tmp_text, sizeof(tmp_text), idx_fmts[tracer->idx_mode], pre, (value8<0)?"-":"", (value8<0)?-value8:value8, post);
+				snprintf(tmp_text, sizeof(tmp_text), idx_fmts[tracer->idx_mode], pre, (uint16_t)(tracer->instr_pc + tracer->nbytes + value8), post);
 				break;
 			case IDX_PCR16: case IDX_EXT16:
-				snprintf(tmp_text, sizeof(tmp_text), idx_fmts[tracer->idx_mode], pre, tracer->value, post);
+				snprintf(tmp_text, sizeof(tmp_text), idx_fmts[tracer->idx_mode], pre, (uint16_t)(tracer->instr_pc + tracer->nbytes + tracer->value), post);
 				break;
 			}
 
--- src/mc6809/mc6809_trace.c
+++ src/mc6809/mc6809_trace.c
@@ -920,8 +920,8 @@ static char const * const idx_fmts[17] = {
 	"%s$%04x,%s%s",
 	"%s*%s",
 	"%sD,%s%s",
-	"%s%s$%02x,PCR%s",
-	"%s$%04x,PCR%s",
+	"%s<$%04x,PCR%s",
+	"%s>$%04x,PCR%s",
 	"%s*%s",
 	"%s$%04X%s",
 	"%s%d,%s%s",
@@ -1229,10 +1229,10 @@ void mc6809_trace_byte(struct mc6809_trace *tracer, uint8_t byte, uint16_t pc) {
 				snprintf(tracer->operand_text, sizeof(tracer->operand_text), idx_fmts[tracer->idx_mode], pre, tracer->value, tracer->idx_reg, post);
 				break;
 			case IDX_PCR8:
-				snprintf(tracer->operand_text, sizeof(tracer->operand_text), idx_fmts[tracer->idx_mode], pre, (value8<0)?"-":"", (value8<0)?-value8:value8, post);
+				snprintf(tracer->operand_text, sizeof(tracer->operand_text), idx_fmts[tracer->idx_mode], pre, (uint16_t)(tracer->instr_pc + tracer->nbytes + value8), post);
 				break;
 			case IDX_PCR16: case IDX_EXT16:
-				snprintf(tracer->operand_text, sizeof(tracer->operand_text), idx_fmts[tracer->idx_mode], pre, tracer->value, post);
+				snprintf(tracer->operand_text, sizeof(tracer->operand_text), idx_fmts[tracer->idx_mode], pre, (uint16_t)(tracer->instr_pc + tracer->nbytes + tracer->value), post);
 				break;
 			case IDX_ILL_A: case IDX_ILL_E:
 				snprintf(tracer->operand_text, sizeof(tracer->operand_text), idx_fmts[tracer->idx_mode], pre, post);
