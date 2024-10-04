3c6ee6a0d957dd3add4ce54607f6f0cdfd595ea6
Julian Brown <julian@the-lair.info>
2024-10-04 20:32:26 +0100

    refine build script@

--- containerbuildapp.sh
+++ containerbuildapp.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 cd /usr/local/xroar
+pwd | echo
+ls -la | echo
+echo "preparing build-w64"
+if [ -d "/usr/local/xroar/build-w64" ]
+then
+  rm -rf /usr/local/xroar/build-w64
+  mkdir /usr/local/xroar/build-w64
+fi
+echo "preparing bin"
+if [ -d "/usr/local/xroar/bin" ]
+then
+  rm -rf /usr/local/xroar/bin
+  mkdir /usr/local/xroar/bin
+fi
+echo "autogen"
 /usr/local/xroar/autogen.sh
+echo "configure"
 /usr/local/xroar/configure
+echo "make native"
 make
+echo "make install native"
 make install
-mkdir bin
 cp /usr/local/bin/xroar ./bin/xroar
+cd /usr/local/xroar/src
 make distclean
-cd build-w64
+cd /usr/local/xroar/build-w64
 ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes ../configure \
 --host=x86_64-w64-mingw32 --prefix=/usr/x86_64-w64-mingw32 \
 --with-sdl-prefix="/usr/x86_64-w64-mingw32" \
--- dockerfile
+++ dockerfile
@@ -1,5 +1,4 @@
 FROM xroar/base:12.7
 WORKDIR /usr/local
-COPY . /usr/local/xroar
 
 CMD cd /usr/local/xroar && /usr/local/xroar/containerbuildapp.sh
