261f79f952120d961270399ad91cc6a0928b96ee
Julian Brown <julian@the-lair.org.uk>
2024-10-05 21:29:40 +0100

    suspend native build

--- containerbuildapp.sh
+++ containerbuildapp.sh
@@ -7,6 +7,7 @@ if [ -d "/usr/local/xroar/build-w64" ]
 then
   rm -rf /usr/local/xroar/build-w64
   mkdir /usr/local/xroar/build-w64
+  mkdir /usr/local/xroar/build-w64/bin
 fi
 echo "preparing bin"
 if [ -d "/usr/local/xroar/bin" ]
@@ -17,14 +18,14 @@ fi
 echo "autogen"
 /usr/local/xroar/autogen.sh
 echo "configure"
-/usr/local/xroar/configure
-echo "make native"
-make
-echo "make install native"
-make install
-cp /usr/local/bin/xroar ./bin/xroar
-cd /usr/local/xroar/src
-make distclean
+#/usr/local/xroar/configure
+#echo "make native"
+#make
+#echo "make install native"
+#make install
+#cp /usr/local/bin/xroar ./bin/xroar
+#cd /usr/local/xroar/src
+#make distclean
 cd /usr/local/xroar/build-w64
 ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes ../configure \
 --host=x86_64-w64-mingw32 --prefix=/usr/x86_64-w64-mingw32 \
@@ -33,5 +34,7 @@ ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes ../configure \
 --without-oss --without-pulse --without-sndfile --without-joydev \
 CFLAGS="-std=c11 -Ofast -flto=auto -D__USE_MINGW_ANSI_STDIO=1" \
 LDFLAGS="-std=c11 -Ofast -flto=auto -D__USE_MINGW_ANSI_STDIO=1"
+echo "make win exe"
 make
+echo "install"
 make install
