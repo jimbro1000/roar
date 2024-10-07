e1963968ecf4c8b270bd2e2e330808277fe65e7f
julian <julian@the-lair.info>
2024-10-07 21:11:12 +0100

    refine dependency builds

--- SDL/build.sh
+++ SDL/build.sh
@@ -1,7 +1,15 @@
 !/bin/sh
+if [ -d "SDL" ]
+then
+  rm -rf SDL
+fi
 echo "clone source"
 git clone https://github.com/libsdl-org/SDL.git
 cd SDL
+git checkout 79ec168f3c1e2fe27335cb8886439f7ef676fb49
+./configure --prefix=/usr/x86_64-w64-mingw32 --host=x86_64-w64-mingw32 \
+    --enable-static --disable-shared \
+    CFLAGS="-Ofast -g" CPPFLAGS="-D__USE_MINGW_ANSI_STDIO=1"
 echo "make"
 cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=build-scripts/cmake-toolchain-mingw64-x86_64.cmake && cmake --build build && cmake --install build
 
--- TRE/build.sh
+++ TRE/build.sh
@@ -10,7 +10,10 @@ git checkout 6092368aabdd0dbb0fbceb2766a37b98e0ff6911
 echo "autogen"
 ./utils/autogen.sh
 echo "configure"
-./configure
+./configure --prefix /usr/x86_64-w64-mingw32 --host=x86_64-w64-mingw32 \
+    --enable-static --disable-shared \
+    --disable-agrep --disable-approx \
+    CFLAGS="-Ofast -g" CPPFLAGS="-D__USE_MINGW_ANSI_STDIO=1"
 echo "make"
 make && make install
 make dist
